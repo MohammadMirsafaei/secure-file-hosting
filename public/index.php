f76fe9ca9504a0df68d6c2003640b86d0f9c4812
Mohammad Mirsafaei <info@mirsafaei.ir>
2020-08-02 08:19:34 +0430

    insert to c

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $app->handle('/login', 'GET', function() use($blade,$static) {
     echo $blade->run('login',['static'=>$static]);
 });
 $app->handle('/login', 'POST', function(Request $request) use($blade,$static) {
-    Database::insert("insert into Users(username,password) values (:username, :password)",
+    Database::command("insert into Users(username,password) values (:username, :password)",
                     [
                         'username' => 'qwe',
                         'password' => '123'
